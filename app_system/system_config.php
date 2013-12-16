a6f2402a8118f506c9e338eea4e0b99b6d785d03
tiamo222 <chalermporn1984@gmail.com>
2013-12-16 10:07:29 +0700

    Update system_config.php

--- app_system/system_config.php
+++ app_system/system_config.php
@@ -29,7 +29,7 @@ ini_set("register_globals", "Off");
 	
 /////////// server
 	$sysconfig["db"]["host"] 		= "ap-cdbr-azure-east-b.cloudapp.net";	
-	$sysconfig["db"]["name"]		= "thinkwebmysqldb";
+	$sysconfig["db"]["name"]		= "thinkwebMySQLDB";
 	$sysconfig["db"]["user"] 		= "b896084e5a9353";
 	$sysconfig["db"]["pws"] 		= "c171d329";
 	
